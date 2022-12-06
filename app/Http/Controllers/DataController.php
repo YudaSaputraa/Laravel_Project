b0d978ccde0da8785f4f65deb7f79647eb7809e5
Yuda Saputra <yudasaputra082@gmail.com>
2022-12-06 20:45:42 +0700

    add file

--- app/Http/Controllers/DataController.php
+++ app/Http/Controllers/DataController.php
@@ -9,34 +9,26 @@ use Illuminate\Http\Request;
 
 class DataController extends Controller
 {
-    public function index()
-    {
+    public function index() {
         $categories = Categories::all();
         return view('index', compact(['categories']));
     }
-
-    public function product($CategoryID)
-    {
+    public function product($CategoryID){
         $product = products::where('CategoryID', $CategoryID)->get();
         return view('productPage', compact(['product']));
     }
-
-    public function productDetail($productID)
-    {
+    public function productDetail($productID){
         $detail = products::where('ProductID', $productID)->get();
         return view('productDetail', compact(['detail']));
     }
-
-    public function sessionData(Request $request)
-    {
+    public function sessionData(Request $request){
         $request->session()->put('productName', $request->productName);
         $request->session()->put('productprice', $request->price);
         $request->session()->put('jumlah', $request->jumlah);
         $request->session()->put('total', $request->jumlah * $request->price);
         return view('cartinput');
     }
-    public function processPostData(Request $request)
-    {
+    public function processPostData(Request $request){
         if ($request->session()->has('productName') && $request->session()->has('productprice') && $request->session()->has('jumlah') && $request->session()->has('total')) {
             return view('cart', compact('request'));
         } else {
